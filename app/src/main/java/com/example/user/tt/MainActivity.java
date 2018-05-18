ee13231acbe135ec454510b50cf897e9776fceb1
tenaciousDeepak <39375058+tenaciousDeepak@users.noreply.github.com>
2018-05-18 18:58:24 +0530

    V1.0- Basic Code

---
+++ app/src/main/java/com/example/user/tt/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.user.tt;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = registerReceiver(null, ifilter);
+
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+
+// How are we charging?
+        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+        int batteryPct = level * 100 / (int) scale;
+
+        TextView tv = findViewById(R.id.batteryText);
+        tv.setText(batteryPct + "%");
+
+        if (isCharging == true) {
+            TextView tv1 = findViewById(R.id.chargeState);
+            if (usbCharge == true) {
+                tv1.setText("Charging Over USB");
+            }
+
+            if (acCharge == true) {
+                tv1.setText("Charging Over AC ");
+            }
+
+        } else {
+            TextView tv1 = findViewById(R.id.chargeState);
+            tv1.setText("Not Charging     ");
+        }
+
+        ProgressBar pv = findViewById(R.id.progressBar2);
+        pv.setProgress(batteryPct);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            Toast.makeText(this, "Refreshed", Toast.LENGTH_SHORT).show();
+            Intent intent = getIntent();
+            overridePendingTransition(0, 0);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            finish();
+            overridePendingTransition(0, 0);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    }
